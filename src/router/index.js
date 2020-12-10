4e8b3052202844776ccb1c1378da689c9f4f3c0c
liwei <15010199100@163.com>
2020-12-10 22:30:20 +0800

    产品页

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 const Index = resolve => require(['@/views/index'], resolve)
+const Product = resolve => require(['@/views/product'], resolve)
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ const router = new Router({
       path: '/',
       name: 'index',
       component: Index
+    },
+    {
+      path: '/product',
+      name: 'Product',
+      component: Product
     },
   ]
 })
---
+++ src/views/product.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- 头部组件 -->
+    <Header></Header>
+    <img src="../image/1.png" alt="" srcset="">
+    <img src="../image/2.png" alt="" srcset="">
+    <img src="../image/3.png" alt="" srcset="">
+    <img src="../image/4.png" alt="" srcset="">
+    <img src="../image/5.png" alt="" srcset="">
+    <img src="../image/6.png" alt="" srcset="">
+    <!-- footer组件 -->
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+export default {
+  components:{
+    Header,
+    Footer
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+  display: block;
+}
+</style>
